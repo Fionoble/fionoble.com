b965daca4da94779b3864a6627278913d943c085
Phiroze Noble <p.noble.dennis@gmail.com>
2021-01-27 22:56:47 -0500

    Working links

--- components/NavItem.js
+++ components/NavItem.js
@@ -1,13 +1,19 @@
 import NavItemStyles from './styles/NavItemStyles';
+import NextLink from 'next/link';
 
 const NavItem = (props) => {
 
 
+  
+  const Link = props.external ? (p => <a {...p}/>) : NextLink;
+
   return (
     <NavItemStyles>
-      <div className="nav-item-title">
-        {props.label}
-      </div>
+      <Link href={props.link}>
+        <div className="nav-item-title">
+          {props.label}
+        </div>
+      </Link>
     </NavItemStyles>
   )
 };
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,9 +4,9 @@ import NavItem from './NavItem';
 const Navigation = () => (
   <NavigationStyles>
     <NavItem link="/" label="Home" image=""/>
-    <NavItem link="https://fionoble.com/blog" label="Blog" image=""/>
+    <NavItem link="https://fionoble.com/blog" external label="Blog" image=""/>
     <NavItem link="/about" label="About" image=""/>
-    <NavItem link="https://fionoble.shop" label="Shop" image=""/>
+    <NavItem link="https://fionoble.shop" external label="Shop" image=""/>
   </NavigationStyles>
 );
  export default Navigation;
